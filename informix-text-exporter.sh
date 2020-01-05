441a9adf17858722a6738abe7f8bbb42ca5e7bfc
Spokey Wheeler <12411310+SpokeyWheeler@users.noreply.github.com>
2020-01-05 15:59:41 +0000

    Revert "Issue 4"

--- informix-text-exporter.sh
+++ informix-text-exporter.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+starttime=$( date +%s%3N )
+
 if [ $# -ne 1 ]
 then
 	echo "
@@ -13,8 +15,6 @@ fi
 
 frq=$1
 
-starttime=$( date +%s )
-
 case $frq in
 	1|2|3|4|5|6|10|12|15|20|30|60)
 		:
@@ -26,9 +26,9 @@ case $frq in
 			;;
 esac
 
-textfile_path=$( jq '.[] | .textfile_path' < config.json 2> /dev/null | tr -d \" )
+textfile_path=$( jq '.[] | .textfile_path' < config.json | tr -d \" )
 
-nummetrics=$( jq '. | length' < metrics.json 2> /dev/null )
+nummetrics=$( jq '. | length' < metrics.json )
 cnt=0
 
 ls -l static_labels > /dev/null 2>&1
@@ -44,122 +44,120 @@ cat /dev/null > "/tmp/informix-text-exporter.$frq.$$"
 while [ "$cnt" -lt "$nummetrics" ]
 do
 	commas=0
-	frequency=$( jq --argjson cnt "$cnt" '.[$cnt] | .frequency' < metrics.json 2> /dev/null | tr -d \" )
+	frequency=$( jq --argjson cnt "$cnt" '.[$cnt] | .frequency' < metrics.json | tr -d \" )
 
 	if [ "$frequency" -eq "$frq" ]
 	then
 
-		metricname=$( jq --argjson cnt "$cnt" '.[$cnt] | .metricname' < metrics.json 2> /dev/null | tr -d \" )
-		help=$( jq --argjson cnt "$cnt" '.[$cnt] | .help' < metrics.json 2> /dev/null | tr -d \" )
-		type=$( jq --argjson cnt "$cnt" '.[$cnt] | .type' < metrics.json 2> /dev/null | tr -d \" )
-		database=$( jq --argjson cnt "$cnt" '.[$cnt] | .database' < metrics.json 2> /dev/null | tr -d \" )
-		sql=$( jq --argjson cnt "$cnt" '.[$cnt] | .sql' < metrics.json 2> /dev/null | tr -d \" )
+		metricname=$( jq --argjson cnt "$cnt" '.[$cnt] | .metricname' < metrics.json | tr -d \" )
+		help=$( jq --argjson cnt "$cnt" '.[$cnt] | .help' < metrics.json | tr -d \" )
+		type=$( jq --argjson cnt "$cnt" '.[$cnt] | .type' < metrics.json | tr -d \" )
+		database=$( jq --argjson cnt "$cnt" '.[$cnt] | .database' < metrics.json | tr -d \" )
+		sql=$( jq --argjson cnt "$cnt" '.[$cnt] | .sql' < metrics.json | tr -d \" )
 
 		echo "# HELP $metricname $help" >> "/tmp/informix-text-exporter.$frq.$$"
 		echo "# TYPE $metricname $type" >> "/tmp/informix-text-exporter.$frq.$$"
 
 		origsql=$sql
 		newsql=$sql
-		subsel=1
-
-		sql=""
 
 		for i in $newsql
 		do
+sleep 1
 			j=$( echo "$i" | tr "[:upper:]" "[:lower:]" )
-			sql="$sql $j"
-			if [ "x$j" == "xunion" ]
+			if [ "x$j" == "xselect" ]
 			then
-				break
-			fi
-			if [ "x${j}" == "x(SELECT" ]
-			then
-				subsel=0
-			fi
-			if [ "x$j" == "xfrom" ]
-			then
-			        if [ $subsel -ne 0 ]
+				sql="$i '${metricname}', "
+			else
+				if [ "x$j" == "xfrom" ]
 				then
 					commas=$( echo "$sql" | while read -r x; do echo "$x" | grep -o "," | wc -l; done )
-					if [ $(( commas % 2 )) -ne 0 ]
+					if [ $(( commas % 2 )) -ne 1 ]
 					then
 						echo "WARNING: label without value or vice-versa"
 					fi
-				else
-					subsel=1
+					sql="$sql, '|'"
 				fi
-		fi
-		done
-
-		sql=$origsql
-
-		dbaccess "$database" <<! 2> /dev/null | grep -v "^$"
-UNLOAD TO /tmp/informix-text-exporter.tmp.$frq.$$
-$sql
-!
-		# if [ $( wc -l /tmp/informix-text-exporter.tmp.$frq.$$ | awk '{print $1}'  ) -eq 0 ]
-		if [ -s "/tmp/informix-text-exporter.tmp.$frq.$$" ]
-		then
-			# echo "OK: $sql"
-			:
-		else
-			echo "Failed: $sql"
-			cnt=$(( cnt + 1 ))
-			continue
-		fi
-		sed -i -e "s/|$//" "/tmp/informix-text-exporter.tmp.$frq.$$"
-		if [ "$commas" -ne 0 ]
-		then
-			sed -i -e "s/|/\"} /$commas" "/tmp/informix-text-exporter.tmp.$frq.$$"
-		fi
-		pipecnt=$( head -1 "/tmp/informix-text-exporter.tmp.$frq.$$" | grep -o "|" | wc -l | awk '{print $1}' )
-		for ((k=1;k<=pipecnt;k++))
-		do
-			if [ $(( k % 2 )) -eq 1 ]
-			then
-				sed -i -e "s/|/=/" "/tmp/informix-text-exporter.tmp.$frq.$$"
-			else
-				sed -i -e "s/|/,/" "/tmp/informix-text-exporter.tmp.$frq.$$"
+				sql="$sql $i"
 			fi
 		done
 
-		if [ "$sl_exists" -eq 0 ]
+		newsql=$origsql
+
+		ccnt=0
+		for i in $newsql
+		do
+sleep 1
+			j=$( echo "$i" | tr "[:upper:]" "[:lower:]" )
+			if [ "x$j" == "xselect" ]
+			then
+				sql="$i '"
+				sql="${sql}${metricname}"
+				sql="${sql}', "
+			else
+				if [ "$ccnt" -eq "$commas" ]
+				then
+					if [ "$commas" -gt 1 ]
+					then
+						sql="$sql'}',"
+					fi
+				fi
+				sql="$sql $i"
+			fi
+			ccnt=$(( ccnt + 1 ))
+		done
+		
+		if [ "$commas" -eq 1 ]
+		then
+			dbaccess "$database" <<! 2> /dev/null | grep -v "^$" | tr -s "[:space:]" "[:space:]">> "/tmp/informix-text-exporter.$frq.$$"
+OUTPUT TO PIPE "cat" WITHOUT HEADINGS
+$sql
+!
+		else
+			pst='paste -d ,= -'
+			ccnt=0
+			while [ "$ccnt" -lt "$commas" ]
+			do
+sleep 1
+				pst="$pst -"
+				ccnt=$(( ccnt + 1 ))
+			done
+echo "$sql"
+			dbaccess "$database" <<! 2> /dev/null | grep -v "^$" | tr -s | eval "$pst"| tr -s "[:space:]" "[:space:]">> "/tmp/informix-text-exporter.$frq.$$"
+OUTPUT TO PIPE "cat" WITHOUT HEADINGS
+$sql
+!
+		fi
+
+		if [ $sl_exists -eq 0 ]
 		then
 			if [ -s static_labels ]
 			then
-				if [ "$commas" -eq 0 ]
+				if [ "$commas" -eq 1 ]
 				then
-					sed -i -e "s/^/$metricname{$statics\"} /" "/tmp/informix-text-exporter.tmp.$frq.$$"
+					sed -i -e "s/^${metricname}/$metricname{$statics\"}/" "/tmp/informix-text-exporter.$frq.$$"
 				else
-					sed -i -e "s/^/$metricname{$statics,/" "/tmp/informix-text-exporter.tmp.$frq.$$"
+					sed -i -e "s/^${metricname}/$metricname{$statics/" "/tmp/informix-text-exporter.$frq.$$"
 				fi
 			fi
 		fi
 	fi
-	cat "/tmp/informix-text-exporter.tmp.$frq.$$" >> "/tmp/informix-text-exporter.$frq.$$"
 
 	cnt=$(( cnt + 1 ))
+exit
 done
-
 sed -i -e 's/} ="/"} /' "/tmp/informix-text-exporter.$frq.$$"
 sed -i -e 's/,}=/"} /' "/tmp/informix-text-exporter.$frq.$$"
 
-echo "# HELP informix_exporter_duration How long the Informix exporter takes to run in seconds" >> "/tmp/informix-text-exporter.$frq.$$"
+echo "# HELP informix_exporter_duration How long the Informix exporter takes to run in milliseconds" >> "/tmp/informix-text-exporter.$frq.$$"
 echo "# TYPE informix_exporter_duration gauge" >> "/tmp/informix-text-exporter.$frq.$$"
 
-endtime=$( date +%s )
+endtime=$( date +%s%3N )
 dur=$(( endtime - starttime ))
 
 echo "informix_exporter_duration{$statics,frequency=$frq\"} $dur" >> "/tmp/informix-text-exporter.$frq.$$"
 sed -i -e 's/,/",/g' "/tmp/informix-text-exporter.$frq.$$"
 sed -i -e 's/=/="/g' "/tmp/informix-text-exporter.$frq.$$"
-
-if promtool check metrics < "/tmp/informix-text-exporter.$frq.$$" > /tmp/metrics.lint.err 2>&1
-then
-	:
-else
-	echo "Prometheus linting error(s). Check /tmp/metrics.lint.err for details"
-fi	
 
 mv "/tmp/informix-text-exporter.$frq.$$" "$textfile_path/informix-text-exporter.$frq.prom"
 rm "${frq}.lock" 2> /dev/null
